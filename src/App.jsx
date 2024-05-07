a508c4fd6df3fa95f1c949fb273fed6717e538d3
SamShep2495 <samsheppard2495@hotmail.com>
2024-05-07 08:59:51 +0100

    I've added an upodated plan.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import {Routes, Route} from "react-router-dom"
+import {ArticleList} from './components/Articles'
+import Header from './components/Header'
+
 
 function App() {
 
   return (
     <>
-      <h1>NC-NEWS</h1>
+      <Header />
+      <Routes>
+        <Route path="/articles" element={ <ArticleList />}/>
+      </Routes>
     </>
   )
 }
